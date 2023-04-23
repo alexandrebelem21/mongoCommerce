69cbd789dfef7ac3225a9983e5b2551dff1fcc34
alexandrebelem21 <alexandrebelem21@gmail.com>
2023-04-22 22:21:17 -0300

    Feat: Desafios 8 e 9. Fix: Lint

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,10 +1,12 @@
-db.produtos.find(
-    {
-        vendidos: {$gt: 50, $lt: 100}
-    },
-    {
-        _id: 0,
-        nome: 1,
-        vendidos: 1
+db.produtos.find({
+    vendidos: {
+        $gt: 50,
+        $lt: 100
     }
-).sort({vendidos: 1})
+}, {
+    _id: 0,
+    nome: 1,
+    vendidos: 1
+}).sort({
+    vendidos: 1
+})
